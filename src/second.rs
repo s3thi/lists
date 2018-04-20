0c8f406d1990c1d90698e11dd2e044a0458fb6d3
Ankur Sethi <contact@ankursethi.in>
2018-04-20 20:07:49 +0530

    Implemented Iter.

--- src/second.rs
+++ src/second.rs
@@ -42,10 +42,6 @@ impl<T> List<T> {
             &mut node.elem
         })
     }
-
-    pub fn into_iter(self) -> IntoIter<T> {
-        IntoIter(self)
-    }
 }
 
 impl<T> Drop for List<T> {
@@ -60,10 +56,36 @@ impl<T> Drop for List<T> {
 
 pub struct IntoIter<T>(List<T>);
 
+impl<T> List<T> {
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+}
+
 impl<T> Iterator for IntoIter<T> {
     type Item = T;
     fn next(&mut self) -> Option<Self::Item> {
         self.0.pop()
+    }
+}
+
+pub struct Iter<'a, T: 'a> {
+    next: Option<&'a Node<T>>,
+}
+
+impl<T> List<T> {
+    pub fn iter(&self) -> Iter<T> {
+        Iter { next: self.head.as_ref().map(|node| &**node) }
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_ref().map(|node| &**node);
+            &node.elem
+        })
     }
 }
 
@@ -118,5 +140,19 @@ mod test {
         assert_eq!(iter.next(), Some(3));
         assert_eq!(iter.next(), Some(2));
         assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn iter() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
     }
 }
